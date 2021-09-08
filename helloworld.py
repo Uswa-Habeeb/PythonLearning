cb7bb29698a76309223ee46a12a67b05b6ec5e39
uswahabeeb <90279611+uswahabeeb@users.noreply.github.com>
2021-09-08 20:53:00 +0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+print('Hello World')
+
+#---------------------
+
+print(nnjnjnn'hello')
+#SyntaxError - Because I have written out of the Quotaion marks
+
+#----------------------
+
+printzsssss('What are you up to')
+#SyntaxError - Because I have written out of the brackets
+
+#----------------------
+
+print('Hey_there')
+
+#----------------------
+
+print('What's up')
+#SyntaxError - Because there should not be that sign in between
+
