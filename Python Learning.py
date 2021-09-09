2b449954a7625bcaa747a72b9b274122c9273f98
uswahabeeb <90279611+uswahabeeb@users.noreply.github.com>
2021-09-09 18:31:15 +0500

    Add files via upload

---
+++ Python Learning.py
@@ -0,0 +1,28 @@
+Firstname = 'Uswa'
+Lastname = 'Habeeb'
+
+print(Firstname,Lastname)
+print(Firstname);(Lastname)
+print(Firstname + Lastname)
+print(Firstname),(Lastname)
+
+
+print(type(Firstname))
+
+print(type(Lastname))
+
+
+first = (9)
+second = (5)
+third = (4)
+
+res = first + second + third
+print(res)
+
+
+
+#Python = var_1
+#var_1 = Python-3
+var_1 = "Python 3.9"
+print(var_1)
+print(type(var_1))
