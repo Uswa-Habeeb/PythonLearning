4d1ad00f61438d44b02eeddb5f02be692cb31279
uswahabeeb <90279611+uswahabeeb@users.noreply.github.com>
2021-09-11 16:03:27 +0500

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,13 @@
+FirstName = "Uswa"
+LastName = "Habeeb"
+Age = 17
+
+print(f"{FirstName}")
+print(f"{LastName}")
+print(f"{Age}")
+
+print(type(FirstName))
+print(type(LastName))
+print(type(Age))
+
+print(f"My name is {FirstName} {LastName}. I am {Age} Years old." )
