7ffb2bf0a77a1a1ca18d0ef98957c59d9eb0225f
uswahabeeb <90279611+uswahabeeb@users.noreply.github.com>
2021-09-14 23:55:59 +0500

    3rd Assignment

--- Assignment.py
+++ Assignment.py
@@ -2,12 +2,25 @@ FirstName = "Uswa"
 LastName = "Habeeb"
 Age = 17
 
+
+
 print(f"{FirstName}")
+# Uswa
 print(f"{LastName}")
+# Habeeb
 print(f"{Age}")
+# 17
+
+
 
 print(type(FirstName))
+# class 'str'
 print(type(LastName))
+# class 'str'
 print(type(Age))
+# class 'int'
+
+
 
 print(f"My name is {FirstName} {LastName}. I am {Age} Years old." )
+# My name is Uswa Habeeb. I am 17 Years old.
