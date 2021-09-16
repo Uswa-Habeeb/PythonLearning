425ac2082ed86bfc45d7ba249161866c0f3930e3
Uswa Habeeb <90279611+uswahabeeb@users.noreply.github.com>
2021-09-16 10:07:36 +0500

    4th Assignment

---
+++ Assignment_4.py
@@ -0,0 +1,176 @@
+# Addition
+Operand_1 = 4
+Operand_2 = 9
+result = Operand_1 + Operand_2
+print(f'{result}')
+# 13
+
+
+# Subtraction
+Operand_1 = 7
+Operand_2 = 10
+result = Operand_1 - Operand_2
+print(f'{result}')
+# -3
+
+
+# Multiplication
+Operand_1 = 6
+Operand_2 = 678
+result = Operand_1 * Operand_2
+print(f'{result}')
+# 4068
+
+
+# Exponentiation
+Operand_1 = 23
+Operand_2 = 6
+result = Operand_1 ** Operand_2
+print(f'{result}')
+# 148035889
+
+
+# Division 
+Operand_1 = 3
+Operand_2 = 87
+result = Operand_1 / Operand_2
+print(f'{result}')
+# 0.034482758620689655
+
+
+# Floor Division
+Operand_1 = 76
+Operand_2 = 9
+result = Operand_1 // Operand_2
+print(f'{result}')
+# 8
+
+
+# Modulus 
+Operand_1 = 15
+Operand_2 = 2
+result = Operand_1 % Operand_2
+print(f'{result}')
+# 1
+
+
+
+
+# 1st Condition
+# Operand_1 > Operand_2
+
+# Addition
+Operand_1 = 10
+Operand_2 = 3
+result = Operand_1 + Operand_2
+print(f'The Result of Addition = {result}')
+# Result of Addition = 13
+
+
+# Subtraction
+Operand_1 = 89
+Operand_2 = 20
+result = Operand_1 - Operand_2
+print(f'The Result of Subtraction = {result}')
+# Result of Subtraction = 69
+
+
+# Multiplication 
+Operand_1 = 89
+Operand_2 = 20
+result = Operand_1 * Operand_2
+print(f'The Result of Multiplication = {result}')
+# Result of Multiplication = 1780
+
+
+# Division
+Operand_1 = 123
+Operand_2 = 67
+result = Operand_1 / Operand_2
+print(f'The Result of Division = {result}')
+# Result of Division = 1.835820895522388
+
+
+# Floor Division
+Operand_1 = 10
+Operand_2 = 3
+result = Operand_1 // Operand_2
+print(f'The Result of Floor Division = {result}')
+# Result of Floor Division = 3
+
+
+# Exponentiation
+Operand_1 = 12
+Operand_2 = 8
+result = Operand_1 ** Operand_2
+print(f'The Result of Exponentiation = {result}')
+# Result of Exponentiation = 429981696
+
+
+# Modulus
+Operand_1 = 60
+Operand_2 = 23
+result = Operand_1 % Operand_2
+print(f'The Result of Modulus = {result}')
+# Result of Modulus = 14
+
+
+
+
+# 2nd Condition
+# Operand_1 < Operand_2
+
+# Addition
+Operand_1 = 3
+Operand_2 = 10
+result = Operand_1 + Operand_2
+print(f'The Result of Addition = {result}')
+# Result of Addition = 13
+
+
+# Subtraction
+Operand_1 = 20
+Operand_2 = 89
+result = Operand_1 - Operand_2
+print(f'The Result of Subtraction = {result}')
+# Result of Subtraction = -69
+
+
+# Multiplication 
+Operand_1 = 20
+Operand_2 = 89
+result = Operand_1 * Operand_2
+print(f'The Result of Multiplication = {result}')
+# Result of Multiplication = 1780
+
+
+# Division
+Operand_1 = 67
+Operand_2 = 123
+result = Operand_1 / Operand_2
+print(f'The Result of Division = {result}')
+# Result of Division = 0.5447154471544715
+
+
+# Floor Division
+Operand_1 = 3
+Operand_2 = 10
+result = Operand_1 // Operand_2
+print(f'The Result of Floor Division = {result}')
+# Result of Floor Division = 0
+
+
+# Exponentiation
+Operand_1 = 2
+Operand_2 = 31
+result = Operand_1 ** Operand_2
+print(f'The Result of Exponentiation = {result}')
+# Result of Exponentiation = 2147483648
+
+
+# Modulus
+Operand_1 = 23
+Operand_2 = 60
+result = Operand_1 % Operand_2
+print(f'The Result of Modulus = {result}')
+# Result of Modulus = 23
