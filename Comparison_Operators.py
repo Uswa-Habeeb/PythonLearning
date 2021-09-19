ba14b617227f9bec43dcef69e26ba98eae4d1737
Uswa Habeeb <90279611+uswahabeeb@users.noreply.github.com>
2021-09-20 01:40:29 +0500

    6th Assignment

---
+++ Comparison_Operators.py
@@ -0,0 +1,70 @@
+# Comparison Operators
+
+# Equal to (==)
+Clients = 35
+Connections = 35
+
+Result = (Clients == Connections)
+
+print(f'Number of Clients {Clients} are Equal to the Number of Connections {Connections}. This statement is {Result}.')
+# Number of Clients 35 are Equal to the Number of Connections 35. This statement is True.
+
+
+
+
+# Greater than or Equal to (>=)
+Income = 58000
+Expenses = 50000
+
+Result = (Income >= Expenses)
+
+print(f'The current Income is {Income} which is Higher than the current Expenses which is {Expenses}. This statement is {Result}.')
+# The current Income is 58000 which is Higher than the current Expenses which is 50000. This statement is True.
+
+
+
+
+# Not Equal to (!=)
+Income = 58000
+Expenses = 50000
+
+Result = (Income != Expenses)
+
+print(f'The current Income is {Income} which is not Equal to the current Expenses which is {Expenses}. This statement is {Result}.')
+# The current Income is 58000 which is not Equal to the current Expenses which is 50000. This statement is True.
+
+
+
+
+# Less than or Equal to (<=)
+Company_Income = 58000
+Company_Expenses = 50000
+ 
+Result = (Company_Income <= Company_Expenses)
+
+print(f'The current Company Income is {Company_Income} which is Lower than the current Company Expenses which is {Company_Expenses}. This statement is {Result}.')
+# The current Company Income is 58000 which is Lower than the current Company Expenses which is 50000. This statement is False.
+
+
+
+
+# Greater Than (>)
+Home_users = 20
+Corrporate_clients = 15
+ 
+Result = (Home_users > Corrporate_clients)
+
+print(f'The Home users are {Home_users} which are more than the Corrporate clients which are {Corrporate_clients}. This statement is {Result}.')
+# The Home users are 20 which are more than the Corrporate clients which are 15. This statement is True.
+
+
+
+
+# Less Than(<)
+Workers = 5
+Company_Directors = 4
+
+Result = (Workers < Company_Directors)
+
+print(f'The Company Workers are {Workers} which are less than the Company Directors which are {Company_Directors}. This statement is {Result}.')
+# The Company Workers are 5 which are more than the Company Directors which are 4. This statement is False.
