327c5c39c235db2d26d244af83094b6a71e0ff54
Uswa Habeeb <90279611+Uswa-Habeeb@users.noreply.github.com>
2021-10-17 18:02:16 +0500

    Variable Types, Decision Making, Basic operators, for loop.

--- QUIZ#01.py
+++ QUIZ#01.py
@@ -19,7 +19,6 @@ if integer_3 > integer_2 and integer_1:
 # Integer #2: 80
 # Integer #3: -85
 # Integer #2 = 80 is the maximum number out of all integers.
-
 # -----------------------------------------------------
 
 # QUESTION - 02:
@@ -35,10 +34,44 @@ if Area == radius ** 2:
 # OUTPUT:
 # Radius of Circle: 9
 # Area = 254.34㎠
-
 # -----------------------------------------------------
 
 # QUESTION - 03:
 # Write a program to calculate percentage of total marks obtained in final term for a student.
+
+# SOLUTION:
+total_marks = 75
+percentage = total_marks % 100
+print(f"Percentage of total marks obtained = {percentage}%")
+
+# OUTPUT:
+# Percentage of total marks obtained = 75%
+# -----------------------------------------------------
+
+# QUESTION - 04:
+# Write a program to print whether the user defined number is a prime number 
+# or not with proper message.
+
+# SOLUTION:
+number = int(input("Enter the number: "))
+if number >= 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(f"{number} is not a prime number")
+            print(f"Because {i} multiply by {number/i} = {number}.")
+            break
+    else:
+        print(number, "is a prime number")
+        print(f"Because {number} is divisible only by itself and 1.")
+
+# OUTPUT:
+# 11 is a prime number
+# Because 11 is divisible only by itself and 1.
+# 8 is not a prime number
+# Because 2 multiply by 4.0 = 8.
+# -----------------------------------------------------
+
+# QUESTION - 05:
+# Write a Python program to check whether an alphabet is a vowel or consonant.
 
 # SOLUTION:
