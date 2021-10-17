c8ebae222ebc7d0f46a78906966770cd926ab6d6
Uswa Habeeb <90279611+Uswa-Habeeb@users.noreply.github.com>
2021-10-17 20:56:06 +0500

    Variable Types, Decision Making, Basic operators, for loop.

--- QUIZ#01.py
+++ QUIZ#01.py
@@ -87,7 +87,7 @@ else:
 # b is Consonant.
 # -----------------------------------------------------
 
-# QUESTION - 04:
+# QUESTION - 06:
 # Write a Python program to sum up two user defined integers. However, if the sum is:
 
 # Below Zero - Display "Bigger number was negative"
@@ -95,5 +95,120 @@ else:
 # Greater than ZERO - Display "Bigger number was positive"
 
 # SOLUTION:
-integer_1 = input("Integer #1: ")
-integer_2 = input("Integer #2: ")
+integer_1 = int(input("Integer #1: "))
+integer_2 = int(input("Integer #2: "))
+print(f"{integer_1} + {integer_2} = {integer_1 + integer_2 }")
+if integer_1 + integer_2 < 0:
+    print(f"Bigger number was negative.")
+if integer_1 + integer_2 == 0:
+    print(f"Both numbers were equally distant on number line on opposite sides.")
+if integer_1 + integer_2 > 0:
+    print(f"Bigger number was positive.")
+
+# OUTPUT:
+# Integer #1: -5
+# Integer #2: 9
+# -5 + 9 = 4
+# Bigger number was positive.
+# Integer #1: -50
+# Integer #2: 8
+# -50 + 8 = -42
+# Bigger number was negative.
+# -----------------------------------------------------
+
+# QUESTION - 07:
+# Write a Python program to check whether
+# two user defined integers are additive inverse of each other.
+
+# SOLUTION:
+integer_1 = int(input("Integer #1: "))
+integer_2 = int(input("Integer #2: "))
+if integer_1 + integer_2 == 0:
+    print(f"{integer_1} and {integer_2} are additive inverse of each other.")
+else:
+    print(f"{integer_1} and {integer_2} are NOT additive inverse of each other.")
+
+# OUTPUT:
+# Integer #1: 4
+# Integer #2: -4
+# 4 and -4 are additive inverse of each other.
+# Integer #1: 9
+# Integer #2: 9
+# 9 and 9 are NOT additive inverse of each other.
+# -----------------------------------------------------
+
+# QUESTION - 08:
+# Write a Python program to check whether 
+# two user defined integers are multiplicative inverse of each other.
+# SOLUTION:
+integer_1 = int(input("Integer #1: "))
+integer_2 = int(input("Integer #2: "))
+if integer_1 * integer_2 == 1:
+    print(f"{integer_1} and {integer_2} are multiplicative inverse of each other.")
+else:
+    print(f"{integer_1} and {integer_2} are NOT multiplicative inverse of each other.")
+# -----------------------------------------------------
+
+# QUESTION - 09:
+# Write a Python program to check a triangle is equilateral, isosceles or scalene.
+# All triangles have three sides.
+# An equilateral triangle is a triangle in which all three sides are equal.
+# A scalene triangle is a triangle that has three unequal sides.
+# An isosceles triangle is a triangle with (at least) two equal sides.
+
+# SOLUTION:
+side_1 = 50
+side_2 = 55
+side_3 = 50
+
+if side_1 == side_2 and side_3:
+    print(f"It is an Equilateral triangle because all three sides are equal.")
+if side_1 == side_2 or side_3:
+    print(f"It is an Isosceles triangle because two sides are equal.")
+else:
+    print(f"It is a scalene triangle because all three sides are unequal.")
+
+# OUTPUT:
+# It is an Isosceles triangle because two sides are equal.
+# -----------------------------------------------------
+
+# QUESTION - 10:
+# Write a Python program to print user defined multiplication table 
+# and also reverse the table.
+
+# SOLUTION:
+# user defined multiplication table:
+Muliplication_table = int(input("Enter your multiplication table : "))
+for counter in range (1, 11):
+    print(f"{Muliplication_table} x {counter} = {Muliplication_table * counter}")
+# OUTPUT:
+# Enter your multiplication table : 6
+# 6 x 1 = 6
+# 6 x 2 = 12
+# 6 x 3 = 18
+# 6 x 4 = 24
+# 6 x 5 = 30
+# 6 x 6 = 36
+# 6 x 7 = 42
+# 6 x 8 = 48
+# 6 x 9 = 54
+# 6 x 10 = 60
+# -----------------------------------------------------
+
+# reverse multiplication table:
+Muliplication_table = int(input("Enter your reverse multiplication table : "))
+for counter in range (10, 0, -1):
+    print(f"{Muliplication_table} x {counter} = {Muliplication_table * counter}")
+# OUTPUT:
+# Enter your reverse multiplication table : 6
+# 6 x 10 = 60
+# 6 x 9 = 54
+# 6 x 8 = 48
+# 6 x 7 = 42
+# 6 x 6 = 36
+# 6 x 5 = 30
+# 6 x 4 = 24
+# 6 x 3 = 18
+# 6 x 2 = 12
+# 6 x 1 = 6
+# -----------------------------------------------------
